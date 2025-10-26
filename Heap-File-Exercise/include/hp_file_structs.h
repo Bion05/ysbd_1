b9d56e3103953a9352e995e7e4257643d24791a0
Geo2005mar <150054696+Geo2005mar@users.noreply.github.com>
2025-10-26 18:09:03 +0200

    Update hp_file_structs.h

--- Heap-File-Exercise/include/hp_file_structs.h
+++ Heap-File-Exercise/include/hp_file_structs.h
@@ -16,12 +16,21 @@
  * @brief Heap file header containing metadata about the file organization
  */
 typedef struct HeapFileHeader {
+    char filetype[8];
+    int record_size;
+    int records_per_blocks;
 } HeapFileHeader;
 
 /**
  * @brief Iterator for scanning through records in a heap file
  */
 typedef struct HeapFileIterator{
+    int file_handle;
+    int current_block;
+    int current_record;
+    HeapFileHeader *header;
+    int id;
 } HeapFileIterator;
+
 
 #endif /* HP_FILE_STRUCTS_H */
