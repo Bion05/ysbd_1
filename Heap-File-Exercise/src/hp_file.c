d88be1cfd32d0ce252bed236e5ffef59bdb16b04
Bion05 <bion.orfanos@gmail.com>
2025-10-27 22:31:03 +0200

    updated hp_file

--- Heap-File-Exercise/src/hp_file.c
+++ Heap-File-Exercise/src/hp_file.c
@@ -32,7 +32,7 @@ int HeapFile_Create(const char* fileName){
   BF_AllocateBlock(file_handle, block);
 
   // Εγγραφές ανα block
-  header.totalRecords = 1;
+  header.totalRecords = 0;
   header.totalBlocks = 1;
   strncpy(header.filetype, "HEAP", sizeof(header.filetype) - 1);
 
@@ -47,40 +47,36 @@ int HeapFile_Create(const char* fileName){
 
   // Κλήσιμο αρχείου
   CALL_BF(BF_CloseFile(file_handle));
-
   return 1;
 }
 
 int HeapFile_Open(const char *fileName, int *file_handle, HeapFileHeader** header_info)
 {
   BF_Block *block;
+  BF_Block_Init(&block);
   // Άνοιγμα του αρχείου
   CALL_BF(BF_OpenFile(fileName, file_handle));
 
   // Φόρτωση του πρώτου block
   CALL_BF(BF_GetBlock(*file_handle, 0, block));
 
-  // Έλεγχος οτι είναι Heap File
+
+  // Αντιγραφή δεδομένων σε δομή HeapFileHeader
+  char *data = BF_Block_GetData(block);
+  *header_info = malloc(sizeof(HeapFileHeader));
+  memcpy(*header_info, data, sizeof(HeapFileHeader));
+  //Έλεγχος οτι είναι Heap File
   if(strcmp((*header_info)->filetype, "HEAP") != 0){
     fprintf(stderr, "HeapFile_Open: Not Heap file\n");
     *header_info = NULL;
     CALL_BF(BF_UnpinBlock(block));
     CALL_BF(BF_CloseFile(*file_handle));
+    BF_Block_Destroy(&block);
     return 0;
   }
-
-  // Αντιγραφή δεδομένων σε δομή HeapFileHeader
-  char *data = BF_Block_GetData(block);
-  *header_info = malloc(sizeof(HeapFileHeader));
-  if(*header_info == NULL){
-    fprintf(stderr, "HeapFile_Open: Memory allocation failed\n");
-    CALL_BF(BF_UnpinBlock(block));    
-    return 0;
-  }
-  memcpy(*header_info, data, sizeof(HeapFileHeader));
 
   CALL_BF(BF_UnpinBlock(block));
-
+  BF_Block_Destroy(&block);
   return 1;
 }
 
@@ -91,47 +87,44 @@ int HeapFile_Close(int file_handle, HeapFileHeader *hp_info)
   if(hp_info != NULL){
     free(hp_info);
   }
-
   return 1;
 }
 
 int HeapFile_InsertRecord(int file_handle, HeapFileHeader *hp_info, const Record record)
 {
   BF_Block *block;
+  BF_Block_Init(&block);
   int blocks_number = hp_info->totalBlocks;
   char *data;
-  unsigned long available_space =(BF_BLOCK_SIZE*blocks_number) - (sizeof(Record)*hp_info->totalRecords) - sizeof(HeapFileHeader);
+  unsigned long available_space = (BF_BLOCK_SIZE*blocks_number) - (sizeof(Record)*hp_info->totalRecords) - sizeof(HeapFileHeader);
   
   // Εντοπισμος του τελευταίου block
   CALL_BF(BF_GetBlock(file_handle,blocks_number-1, block));
   data = BF_Block_GetData(block);
   // Αν υπάρχει χώρος προσθέτουμε την εγγραφή
-  if(sizeof(record)<=available_space){
+  if(sizeof(Record)<=available_space){
     data += (BF_BLOCK_SIZE - available_space);
-    Record* slot = (Record *)data;
-    *slot = record;
+    memcpy(data,&record,sizeof(Record));
     BF_Block_SetDirty(block);
     CALL_BF(BF_UnpinBlock(block));
     hp_info->totalRecords+=1;
+    BF_Block_Destroy(&block);
     return 1;
   }
 
   // Αν το τελευταίο block είναι γεμάτο δημιουργούμε νέο block
   CALL_BF(BF_UnpinBlock(block));
-
-  BF_Block_Init(&block);
   CALL_BF(BF_AllocateBlock(file_handle, block));
   data = BF_Block_GetData(block);
-  Record* slot = (Record *)data;
-  *slot = record;
+  memset(data, 0, BF_BLOCK_SIZE);
+  memcpy(data,&record,sizeof(Record));
   BF_Block_SetDirty(block);
   CALL_BF(BF_UnpinBlock(block));
   hp_info->totalBlocks+=1;
   hp_info->totalRecords+=1;
+  BF_Block_Destroy(&block);
   return 1;
-
 }
-
 
 HeapFileIterator HeapFile_CreateIterator(    int file_handle, HeapFileHeader* header_info, int id)
 {
@@ -147,20 +140,23 @@ HeapFileIterator HeapFile_CreateIterator(    int file_handle, HeapFileHeader* he
 
 int HeapFile_GetNextRecord(    HeapFileIterator* heap_iterator, Record** record)
 {
-  if(heap_iterator->header->totalRecords==0)return 0;
-  
+  if(heap_iterator->header->totalRecords==0){
+    *record = NULL;
+    return 0;
+  }
   int initial_block = heap_iterator->current_block;
   int initial_record = heap_iterator->current_record;
   
   BF_Block *block; 
+  BF_Block_Init(&block);
   CALL_BF(BF_GetBlock(heap_iterator->file_handle,heap_iterator->current_block,block)); 
   char *data = BF_Block_GetData(block);
-
+  
   if(heap_iterator->current_block==0) data += sizeof(HeapFileHeader);
   Record *records = (Record*) data;
   *record = &records[heap_iterator->current_record];
   heap_iterator->current_record++;
-  CALL_BF(BF_UnpinBlock(block));
+  CALL_BF(BF_UnpinBlock(block));  
 
   while(heap_iterator->current_block<heap_iterator->header->totalBlocks){
     int rpb0full = (BF_BLOCK_SIZE - sizeof(HeapFileHeader)) / sizeof(Record);
@@ -174,15 +170,18 @@ int HeapFile_GetNextRecord(    HeapFileIterator* heap_iterator, Record** record)
       if(records[i].id == heap_iterator->id){ // && records[i].id == -1 if we implement a no filter option 
         heap_iterator->current_record = i;
         CALL_BF(BF_UnpinBlock(block));
+        BF_Block_Destroy(&block);
         return 1;
       }
     }
     heap_iterator->current_block++;
-    heap_iterator->current_record == 0;
+    heap_iterator->current_record = 0;
     CALL_BF(BF_UnpinBlock(block));
   }
   heap_iterator->current_block = initial_block;
   heap_iterator->current_record = initial_record;
+  BF_Block_Destroy(&block);
+  *record = NULL;
   return 0;
 }
 
